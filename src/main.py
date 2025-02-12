11daec4ec0e89825584f0ea6110480a168edd0e8
Piotr Stradowski <stradowski.stradek@gmail.com>
2025-02-12 23:03:32 +0100

    Add positional encoding to transformer encoder

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
+import math
 import os
 from typing import Union
-import numpy
+import numpy as np
 import pickle
 
 
 class TransformerEncoder:
     def __init__(self):
-        self.encoding_lookup_table = None
-        self.decoding_lookup_table = None
+        self.embedding_dimension = 128
+        self.max_sequence_length = 2048
+
+        self.char_encoding_lookup_table = None
+        self.char_decoding_lookup_table = None
+        self.embedding_decoding_lookup_table = None
+        self.embedding_matrix = None
+        self.positional_encoding_matrix = None
 
         self.reserved_encodings = {
             "unknown_char": {
@@ -15,50 +22,99 @@ class TransformerEncoder:
                 "decoded": "<UNK>"
             },
         }
-
         self.reserved_encodings_count = len(self.reserved_encodings)
+    
 
     def build_vocabulary(self, training_text: str) -> Union[dict[str, int], dict[int, str]]:
-        # create encoding lookup table
-        self.encoding_lookup_table = {}
+        # build encoding lookup table
+        self.char_encoding_lookup_table = {}
         for i, char in enumerate(sorted(set(training_text))):
             uid = i + self.reserved_encodings_count
-            self.encoding_lookup_table[char] = uid
+            self.char_encoding_lookup_table[char] = uid
 
-        # create decoding lookup table
-        self.decoding_lookup_table = {}
-        for char, uid in self.encoding_lookup_table.items():
-            self.decoding_lookup_table[uid] = char
+        # build decoding lookup table
+        self.char_decoding_lookup_table = {}
+        for char, uid in self.char_encoding_lookup_table.items():
+            self.char_decoding_lookup_table[uid] = char
 
         # add reserved unknown character
         for name, data in self.reserved_encodings.items():
             uid = data["encoded"]
             char = data["decoded"]
-            self.decoding_lookup_table[uid] = char
+            self.char_encoding_lookup_table[uid] = char
+            self.char_decoding_lookup_table[uid] = char
+
+        vocabulary_size = len(self.char_encoding_lookup_table)
+
+        # initialize embeddings_matrix with random values
+        embedding_random_value_max = 1 / math.sqrt(self.embedding_dimension)
+        embedding_random_value_min = -embedding_random_value_max
+
+        self.embedding_matrix = np.random.uniform(
+            low=embedding_random_value_min,
+            high=embedding_random_value_max,
+            size=(vocabulary_size, self.embedding_dimension)
+        )
+
+        # build embedding decoding lookup table
+        self.embedding_decoding_lookup_table = {}
+        for i, vector in enumerate(self.embedding_matrix):
+            self.embedding_decoding_lookup_table[tuple(vector)] = i
+
+        # build positional encoding matrix
+        self.positional_encoding_matrix = np.zeros((self.max_sequence_length, self.embedding_dimension))
+        for x in range(self.max_sequence_length):
+            for y in range(self.embedding_dimension):
+                if y%2 == 0:
+                    self.positional_encoding_matrix[x][y] = math.sin(x / (10000 ** (2 * y / self.embedding_dimension)))
+                else:
+                    self.positional_encoding_matrix[x][y] = math.cos(x / (10000 ** (2 * y / self.embedding_dimension)))
 
 
-    def encode(self, text: str):
-        encoded_training_text_vector = []
-        for char in text:
+    def get_vocabulary_size(self):
+        return len(self.char_encoding_lookup_table)
+
+
+    def find_embedding_vector_id(self, embedding_vector: np.ndarray) -> int:
+        return self.embedding_decoding_lookup_table.get(tuple(embedding_vector), -1)
+
+
+    def encode(self, text: str, use_positional_encoding=True) -> np.ndarray:
+        if use_positional_encoding:
+            print("WARNING! Encoding with positional encoding.")
+        else:
+            print("WARNING! Encoding without positional encoding.")
+
+        encoded_training_text = np.zeros((len(text), self.embedding_dimension))
+        for i, char in enumerate(text):
             try:
-                encoded_character = self.encoding_lookup_table[char]
+                char_id = self.char_encoding_lookup_table[char]
+
+                embedding_vector = self.embedding_matrix[char_id] 
+                if use_positional_encoding:
+                    embedding_vector += self.positional_encoding_matrix[i]
             except KeyError as e:
                 print(f"KeyError: character not found in the encoding lookup table: {e}")
-                encoded_character = self.reserved_encodings["unknown_char"]["encoded"]
+                unknown_char_id = self.reserved_encodings["unknown_char"]["encoded"]
+                
+                embedding_vector = self.embedding_matrix[unknown_char_id]
+                if use_positional_encoding:
+                    embedding_vector += self.positional_encoding_matrix[i]
 
-            encoded_training_text_vector.append(encoded_character)
+            encoded_training_text[i] = embedding_vector
 
-        return encoded_training_text_vector
+        return encoded_training_text
 
     def decode(self, encoded_text: list[int]):
         decoded_text = ""
-        for char_uid in encoded_text:
+        for embedding_vector in encoded_text:
             try:
-                decoded_character = self.decoding_lookup_table[char_uid]
+                char_id = self.find_embedding_vector_id(embedding_vector)
+                decoded_character = self.char_decoding_lookup_table[char_id]
             except KeyError as e:
                 print(f"KeyError: character not found in the decoding lookup table: {e}")
                 unknown_char_id = self.reserved_encodings["unknown_char"]["encoded"]
-                decoded_character = self.decoding_lookup_table[unknown_char_id]
+                decoded_character = self.char_decoding_lookup_table[unknown_char_id]
 
             decoded_text += decoded_character
 
@@ -66,8 +122,8 @@ class TransformerEncoder:
 
     def save_encodings(self, filepath: str):
         encodings = {
-            "encoding_lookup_table": self.encoding_lookup_table,
-            "decoding_lookup_table": self.decoding_lookup_table,
+            "encoding_lookup_table": self.char_encoding_lookup_table,
+            "decoding_lookup_table": self.char_decoding_lookup_table,
             "reserved_encodings": self.reserved_encodings
         }
         with open(filepath, "wb") as file:
@@ -76,10 +132,15 @@ class TransformerEncoder:
     def load_encodings(self, filepath: str):
         with open(filepath, "rb") as file:
             encodings = pickle.load(file)
-            self.encoding_lookup_table = encodings["encoding_lookup_table"]
-            self.decoding_lookup_table = encodings["decoding_lookup_table"]
+            self.char_encoding_lookup_table = encodings["encoding_lookup_table"]
+            self.char_decoding_lookup_table = encodings["decoding_lookup_table"]
             self.reserved_encodings = encodings["reserved_encodings"]
 
+
+def test_encoding_and_decoding(transformer_encoder: TransformerEncoder, training_text: str):
+    encoded_text = transformer_encoder.encode(training_text, use_positional_encoding=False)
+    decoded_text = transformer_encoder.decode(encoded_text)
+    assert training_text == decoded_text
 
 def main():
     working_dir = os.getcwd()
@@ -92,7 +153,7 @@ def main():
     # load training text
     with open(training_dataset_path, "r", encoding="utf-8") as file:
         training_text = file.read()
-        print(training_text)
+        # print(training_text)
 
     if os.path.exists(encodings_file):
         load_encodings_file = input("Do you want to load existing encodings file? (y/n) ") == "y"
@@ -108,13 +169,12 @@ def main():
         transformer_encoder.build_vocabulary(training_text)
         transformer_encoder.save_encodings(encodings_file)
 
-    encoded_text = transformer_encoder.encode(training_text)
-    print(encoded_text)
+    # test encoding and decoding
+    test_encoding_and_decoding(transformer_encoder, training_text)
 
-    decoded_text = transformer_encoder.decode(encoded_text)
-    print(decoded_text)
+    # embedding layer
+    # TODO: Implement Xavier/Glorot embedding matrix initialization 
 
-    assert training_text == decoded_text
 
 
 
