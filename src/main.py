6bb3697bc43d1824bf6d0379ef29b103f999dc62
Piotr Stradowski <stradowski.stradek@gmail.com>
2025-03-23 20:24:14 +0100

    Clean up

--- src/main.py
+++ src/main.py
@@ -6,28 +6,14 @@ import numpy as np
 import pickle
 
 TRAINING_CHUNK_SIZE = 128
-ENCODER_EMBEDDING_DIMENSION = 128
+TRANSFORMER_EMBEDDING_DIMENSION_SIZE = 128
 ENCODER_MAX_SEQUENCE_LENGTH = 2048
+TRANSFORMER_HEADS_COUNT = 8
+TRANSFORMER_HIDDEN_DIMENSION_SIZE = TRANSFORMER_EMBEDDING_DIMENSION_SIZE * 4
 
-def initialize_random_weight_matrix(size):
-    dim_sum = np.sum(size)
-    limit = np.sqrt(6 / dim_sum)
-    weight_matrix = np.random.uniform(-limit, limit, size)
-    return weight_matrix
-
-def softmax(input_matrix):
-    return np.exp(input_matrix) / np.sum(np.exp(input_matrix), axis=1, keepdims=True)
-    
 class TransformerEncoder:
-    def __init__(self):
-        self.embedding_dimension = ENCODER_EMBEDDING_DIMENSION
+    def __init__(self, training_text: str = ""):
         self.max_sequence_length = ENCODER_MAX_SEQUENCE_LENGTH
-
-        self.char_encoding_lookup_table = None
-        self.char_decoding_lookup_table = None
-        self.embedding_decoding_lookup_table = None
-        self.embedding_matrix = None
-        self.positional_encoding_matrix = None
 
         self.reserved_encodings = {
             "unknown_char": {
@@ -37,63 +23,63 @@ class TransformerEncoder:
         }
         self.reserved_encodings_count = len(self.reserved_encodings)
 
-    def get_embedding_dimension(self):
-        return self.embedding_dimension
-    
+        if training_text != "":
+            self.char_encoding_table = self.build_character_encoding_lookup_table(training_text)
+            self.char_decoding_table = self.build_character_decoding_lookup_table()
+            self.add_reserved_characters_to_encoding_decoding_table()
 
-    def get_max_sequence_length(self):
-        return self.max_sequence_length
-    
+            self.vocabulary_size = len(self.char_encoding_table)
 
-    def build_vocabulary(self, training_text: str) -> Union[dict[str, int], dict[int, str]]:
-        # build encoding lookup table
-        self.char_encoding_lookup_table = {}
-        for i, char in enumerate(sorted(set(training_text))):
-            uid = int(i + self.reserved_encodings_count)
-            self.char_encoding_lookup_table[char] = uid
+            embedding_random_value_max = 1 / math.sqrt(TRANSFORMER_EMBEDDING_DIMENSION_SIZE)
+            embedding_random_value_min = -embedding_random_value_max
 
-        # build decoding lookup table
-        self.char_decoding_lookup_table = {}
-        for char, uid in self.char_encoding_lookup_table.items():
-            self.char_decoding_lookup_table[uid] = char
+            self.embedding_matrix = np.random.uniform(
+                low=embedding_random_value_min,
+                high=embedding_random_value_max,
+                size=(self.vocabulary_size, TRANSFORMER_EMBEDDING_DIMENSION_SIZE)
+            )
 
-        # add reserved unknown character
+            # build embedding decoding lookup table
+            self.embedding_decoding_lookup_table = {}
+            for i, vector in enumerate(self.embedding_matrix):
+                self.embedding_decoding_lookup_table[tuple(vector)] = i
+
+            # build positional encoding matrix
+            self.positional_encoding_matrix = np.zeros((self.max_sequence_length, TRANSFORMER_EMBEDDING_DIMENSION_SIZE))
+            for x in range(self.max_sequence_length):
+                for y in range(TRANSFORMER_EMBEDDING_DIMENSION_SIZE):
+                    if y%2 == 0:
+                        self.positional_encoding_matrix[x][y] = math.sin(x / (10000 ** (y / TRANSFORMER_EMBEDDING_DIMENSION_SIZE)))
+                    else:
+                        self.positional_encoding_matrix[x][y] = math.cos(x / (10000 ** (y / TRANSFORMER_EMBEDDING_DIMENSION_SIZE)))
+
+    def add_reserved_characters_to_encoding_decoding_table(self):
         for name, data in self.reserved_encodings.items():
             uid = data["encoded"]
             char = data["decoded"]
-            self.char_encoding_lookup_table[uid] = char
-            self.char_decoding_lookup_table[uid] = char
+            self.char_encoding_table[uid] = char
+            self.char_decoding_table[uid] = char
 
-        self.vocabulary_size = len(self.char_encoding_lookup_table)
+    def build_character_encoding_lookup_table(self, training_text):
+        char_encoding_table = {}
+        for i, char in enumerate(sorted(set(training_text))):
+            uid = int(i + self.reserved_encodings_count)
+            char_encoding_table[char] = uid
 
-        # initialize embeddings_matrix with random values
-        embedding_random_value_max = 1 / math.sqrt(self.embedding_dimension)
-        embedding_random_value_min = -embedding_random_value_max
+        return char_encoding_table
 
-        self.embedding_matrix = np.random.uniform(
-            low=embedding_random_value_min,
-            high=embedding_random_value_max,
-            size=(self.vocabulary_size, self.embedding_dimension)
-        )
+    def build_character_decoding_lookup_table(self):
+        char_decoding_table = {}
+        for char, uid in self.char_encoding_table.items():
+            char_decoding_table[uid] = char
 
-        # build embedding decoding lookup table
-        self.embedding_decoding_lookup_table = {}
-        for i, vector in enumerate(self.embedding_matrix):
-            self.embedding_decoding_lookup_table[tuple(vector)] = i
+        return char_decoding_table
 
-        # build positional encoding matrix
-        self.positional_encoding_matrix = np.zeros((self.max_sequence_length, self.embedding_dimension))
-        for x in range(self.max_sequence_length):
-            for y in range(self.embedding_dimension):
-                if y%2 == 0:
-                    self.positional_encoding_matrix[x][y] = math.sin(x / (10000 ** (y / self.embedding_dimension)))
-                else:
-                    self.positional_encoding_matrix[x][y] = math.cos(x / (10000 ** (y / self.embedding_dimension)))
-
+    def get_vocabulary_size(self):
+        return self.vocabulary_size
 
     def find_embedding_vector_id(self, embedding_vector: np.ndarray) -> int:
         return self.embedding_decoding_lookup_table.get(tuple(embedding_vector), -1)
-
 
     def encode(self, text: str, use_positional_encoding=True) -> np.ndarray:
         if use_positional_encoding:
@@ -101,10 +87,10 @@ class TransformerEncoder:
         else:
             print("WARNING! Encoding without positional encoding.")
 
-        encoded_training_text = np.zeros((len(text), self.embedding_dimension))
+        encoded_training_text = np.zeros((len(text), TRANSFORMER_EMBEDDING_DIMENSION_SIZE))
         for i, char in enumerate(text):
             try:
-                char_id = self.char_encoding_lookup_table[char]
+                char_id = self.char_encoding_table[char]
 
                 embedding_vector = self.embedding_matrix[char_id] 
                 if use_positional_encoding:
@@ -125,7 +111,7 @@ class TransformerEncoder:
         token_ids = np.zeros(len(text), dtype=int)
         for i, char in enumerate(text):
             try:
-                char_id = self.char_encoding_lookup_table[char]
+                char_id = self.char_encoding_table[char]
             except KeyError as e:
                 print(f"KeyError: character not found in the encoding lookup table: {e}")
                 char_id = self.reserved_encodings["unknown_char"]["encoded"]
@@ -139,11 +125,11 @@ class TransformerEncoder:
         for embedding_vector in encoded_text:
             try:
                 char_id = self.find_embedding_vector_id(embedding_vector)
-                decoded_character = self.char_decoding_lookup_table[char_id]
+                decoded_character = self.char_decoding_table[char_id]
             except KeyError as e:
                 print(f"KeyError: character not found in the decoding lookup table: {e}")
                 unknown_char_id = self.reserved_encodings["unknown_char"]["encoded"]
-                decoded_character = self.char_decoding_lookup_table[unknown_char_id]
+                decoded_character = self.char_decoding_table[unknown_char_id]
 
             decoded_text += decoded_character
 
@@ -151,25 +137,28 @@ class TransformerEncoder:
 
     def get_transformer_encoding_data(self, filepath: str):
         data = {
-            "encoding_lookup_table": self.char_encoding_lookup_table,
-            "decoding_lookup_table": self.char_decoding_lookup_table,
-            "reserved_encodings": self.reserved_encodings
+            "encoding_lookup_table": self.char_encoding_table,
+            "decoding_lookup_table": self.char_decoding_table,
+            "vocabulary_size": self.vocabulary_size,
+            "reserved_encodings": self.reserved_encodings,
+            "reserved_encodings_count": self.reserved_encodings_count,
+            "embedding_matrix": self.embedding_matrix,
+            "embedding_decoding_lookup_table": self.embedding_decoding_lookup_table,
+            "positional_encoding_matrix": self.positional_encoding_matrix
         }
         return data
 
     def load_encodings(self, filepath: str):
         with open(filepath, "rb") as file:
             encodings = pickle.load(file)
-            self.char_encoding_lookup_table = encodings["encoding_lookup_table"]
-            self.char_decoding_lookup_table = encodings["decoding_lookup_table"]
+            self.char_encoding_table = encodings["encoding_lookup_table"]
+            self.char_decoding_table = encodings["decoding_lookup_table"]
+            self.vocabulary_size = encodings["vocabulary_size"]
             self.reserved_encodings = encodings["reserved_encodings"]
-
-
-def test_encoding_and_decoding(transformer_encoder: TransformerEncoder, training_text: str):
-    encoded_text = transformer_encoder.encode(training_text, use_positional_encoding=False)
-    decoded_text = transformer_encoder.decode(encoded_text)
-    assert training_text == decoded_text
-
+            self.reserved_encodings_count = encodings["reserved_encodings_count"]
+            self.embedding_matrix = encodings["embedding_matrix"]
+            self.embedding_decoding_lookup_table = encodings["embedding_decoding_lookup_table"]
+            self.positional_encoding_matrix = encodings["positional_encoding_matrix"]
 
 class LayerNorm:
     def __init__(self):
@@ -183,13 +172,11 @@ class LayerNorm:
 
         return (input_matrix - mean) / np.sqrt(variance + epsilon) * self.gamma + self.beta
 
-
 class SelfAttentionLayer:
-    def __init__(self, embedding_dimension, heads_count):
+    def __init__(self, heads_count):
         self.heads_count = heads_count
-        self.embedding_dimension = embedding_dimension
-        self.head_dim = embedding_dimension // heads_count
-        weight_matrix_size = (embedding_dimension, embedding_dimension)
+        self.head_dim = TRANSFORMER_EMBEDDING_DIMENSION_SIZE // heads_count
+        weight_matrix_size = (TRANSFORMER_EMBEDDING_DIMENSION_SIZE, TRANSFORMER_EMBEDDING_DIMENSION_SIZE)
 
         self.W_q = initialize_random_weight_matrix(weight_matrix_size)
         self.W_k = initialize_random_weight_matrix(weight_matrix_size)
@@ -233,18 +220,17 @@ class SelfAttentionLayer:
 
         concatenated_attention_output = np.concatenate(attention_outputs, axis=-1)
 
-        assert concatenated_attention_output.shape[-1] == self.embedding_dimension
+        assert concatenated_attention_output.shape[-1] == TRANSFORMER_EMBEDDING_DIMENSION_SIZE
 
         return np.dot(concatenated_attention_output, self.W_o)
 
-
 class FeedForwardLayer: 
-    def __init__(self, embedding_dimension_size, hidden_dimension_size):
+    def __init__(self, hidden_dimension_size):
         self.hidden_dimension_size = hidden_dimension_size
-        self.W_1 = initialize_random_weight_matrix((embedding_dimension_size, self.hidden_dimension_size))
-        self.W_2 = initialize_random_weight_matrix((self.hidden_dimension_size, embedding_dimension_size))
+        self.W_1 = initialize_random_weight_matrix((TRANSFORMER_EMBEDDING_DIMENSION_SIZE, self.hidden_dimension_size))
+        self.W_2 = initialize_random_weight_matrix((self.hidden_dimension_size, TRANSFORMER_EMBEDDING_DIMENSION_SIZE))
         self.b_1 = np.zeros(self.hidden_dimension_size)
-        self.b_2 = np.zeros(embedding_dimension_size)
+        self.b_2 = np.zeros(TRANSFORMER_EMBEDDING_DIMENSION_SIZE)
 
     def forward(self, input_matrix):
         H = input_matrix @ self.W_1 + self.b_1
@@ -265,10 +251,9 @@ class FeedForwardLayer:
         inner = k_beta * (input_tensor + k_kappa * input_tensor_cube)
         return input_tensor * 0.5 * (1 + np.tanh(inner))
 
-
 class OutputProjectionLayer:
-    def __init__(self, embedding_dimension_size, vocab_size):
-        self.W_vocab = initialize_random_weight_matrix((embedding_dimension_size, vocab_size))
+    def __init__(self, vocab_size):
+        self.W_vocab = initialize_random_weight_matrix((TRANSFORMER_EMBEDDING_DIMENSION_SIZE, vocab_size))
         self.b_vocab = np.zeros(vocab_size)
 
     def forward(self, input_matrix):
@@ -283,6 +268,43 @@ class OutputProjectionLayer:
     def predicted_token_probabilities_to_tokens(token_probabilities):
         return np.argmax(token_probabilities, axis=-1)
 
+def test_encoding_and_decoding(transformer_encoder: TransformerEncoder, training_text: str):
+    encoded_text = transformer_encoder.encode(training_text, use_positional_encoding=False)
+    decoded_text = transformer_encoder.decode(encoded_text)
+    assert training_text == decoded_text
+
+def export_transformer_encodings(encodings_file, transformer_encoder):
+    encoding_data = transformer_encoder.get_transformer_encoding_data(encodings_file)
+    with open(encodings_file, "wb") as file:
+        pickle.dump(encoding_data, file)
+
+def initialize_random_weight_matrix(size):
+    dim_sum = np.sum(size)
+    limit = np.sqrt(6 / dim_sum)
+    weight_matrix = np.random.uniform(-limit, limit, size)
+    return weight_matrix
+
+def softmax(input_matrix):
+    return np.exp(input_matrix) / np.sum(np.exp(input_matrix), axis=1, keepdims=True)
+
+def split_training_text(training_text):
+    training_text_length = len(training_text)
+
+    training_tokens_chunk_list = []
+    for training_chunk_index in range(training_text_length // TRAINING_CHUNK_SIZE):
+        chunk_start = training_chunk_index*TRAINING_CHUNK_SIZE
+        chunk_end = min(chunk_start + TRAINING_CHUNK_SIZE, training_text_length)
+        text_chunk = training_text[chunk_start:chunk_end]
+
+        training_tokens_chunk_list.append(text_chunk)
+
+    return training_tokens_chunk_list
+
+def load_training_text(training_dataset_path):
+    with open(training_dataset_path, "r", encoding="utf-8") as file:
+        training_text = file.read()
+    
+    return training_text
 
 def main():
     parser = argparse.ArgumentParser(description="Transformer Encoder")
@@ -294,80 +316,50 @@ def main():
     user_data_dir = os.path.join(working_dir, "user_data")
     encodings_file = args.load_encodings if args.load_encodings else os.path.join(user_data_dir, "encodings_data.dat")
 
-    transformer_encoder = TransformerEncoder()
-
-    # load training text
-    with open(training_dataset_path, "r", encoding="utf-8") as file:
-        training_text = file.read()
-        # print(training_text)
+    training_text = load_training_text(training_dataset_path)
 
     # build or load encodings
     if args.load_encodings:
         print(f"Loading encodings from file {encodings_file}...")
+        transformer_encoder = TransformerEncoder()
         transformer_encoder.load_encodings(encodings_file)
     else:
         print("Building encodings from scratch...")
-        transformer_encoder.build_vocabulary(training_text)
+        transformer_encoder = TransformerEncoder(training_text)
 
-        encoding_data = transformer_encoder.get_transformer_encoding_data(encodings_file)
-        with open(encodings_file, "wb") as file:
-            pickle.dump(encoding_data, file)
+        export_transformer_encodings(encodings_file, transformer_encoder)
 
-    # test encoding and decoding
     test_encoding_and_decoding(transformer_encoder, training_text)
 
-    training_text_length = len(training_text)
+    vocab_size = transformer_encoder.get_vocabulary_size()
 
-    # generate training data chunks
-    training_tokens_chunk_list = []
-    for training_chunk_index in range(training_text_length // TRAINING_CHUNK_SIZE):
-        chunk_start = training_chunk_index*TRAINING_CHUNK_SIZE
-        chunk_end = min(chunk_start + TRAINING_CHUNK_SIZE, training_text_length)
-        text_chunk = training_text[chunk_start:chunk_end]
-
-        training_tokens_chunk_list.append(text_chunk)
-
-    # initialize layer norm
     layer_norm = LayerNorm()
+    self_attention_layer = SelfAttentionLayer(TRANSFORMER_HEADS_COUNT)
+    feedforward_layer = FeedForwardLayer(TRANSFORMER_HIDDEN_DIMENSION_SIZE)
+    output_projection_layer = OutputProjectionLayer(vocab_size)
 
-    # initialize self-attention layer
-    heads_count = 8
-    embedding_dimension = transformer_encoder.get_embedding_dimension()
-    vocab_size = transformer_encoder.vocabulary_size
-    
-    self_attention_layer = SelfAttentionLayer(embedding_dimension, heads_count)
-
-    # initialize feed-forward layer
-    hidden_dimension = embedding_dimension * 4
-
-    feedforward_layer = FeedForwardLayer(embedding_dimension, hidden_dimension)
-    
-    # initialize output projection layer
-    output_projection_layer = OutputProjectionLayer(embedding_dimension, vocab_size)
+    training_tokens_chunk_list = split_training_text(training_text)
 
     for training_chunk_index, training_target_tokens in enumerate(training_tokens_chunk_list):
-        # encode input to embeddings
         input_embeddings = transformer_encoder.encode(training_target_tokens)
-        input_sequence_length = input_embeddings.shape[0]
 
-        # forward through self-attention layer
         self_attention_layer_output = self_attention_layer.forward(input_embeddings)
         # self_attention_layer_output = layer_norm.forward(self_attention_layer_output)
         
-        # forward through feed-forward layer
         ffn_output = feedforward_layer.forward(self_attention_layer_output)
         # ffn_output = layer_norm.forward(self_attention_layer_output)
 
-        # forward through output projection layer (projection to vocabulary space)
         logits = output_projection_layer.forward(ffn_output)
         predicted_token_probabilities = OutputProjectionLayer.logits_to_predicted_token_probabilities(logits)
         predicted_tokens = OutputProjectionLayer.predicted_token_probabilities_to_tokens(logits)
 
+        #############################
         # training
+        #############################
         target_tokens = training_target_tokens[1:]
         training_target_token_id = transformer_encoder.encode_to_token_id(target_tokens)
-        
         mask = np.ones(predicted_token_probabilities.shape[0])  
+        predictions_num = predicted_tokens.shape[0]
 
         if training_chunk_index < len(training_tokens_chunk_list) - 1:
             # add first token from next batch to target_tokens
@@ -387,12 +379,11 @@ def main():
             training_target_token_id = np.append(training_target_token_id, 0)
             mask[-1] = 0
 
-        predictions_num = predicted_tokens.shape[0]
-
         one_hot_ground_truth = np.zeros(predicted_token_probabilities.shape)
         one_hot_ground_truth[np.arange(predictions_num), training_target_token_id] = 1
-
+        
         epsilon = np.finfo(float).eps
+
         loss = -np.mean(np.sum(one_hot_ground_truth * np.log(predicted_token_probabilities + epsilon)))
 
         # # prepare AdamW tensor buffers for backward pass
